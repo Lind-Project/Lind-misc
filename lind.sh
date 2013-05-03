62d4c406e3dc65cf78529054fc2daad1077fc58f
psaiteja <lind@ubuntu.(none)>
2013-05-02 18:56:27 -0700

    modified the install() in b script so as to copy all the needed files in the new Ubuntu VM. The other functions are untouched for now, but they need to be changed too to automate the setup process. The lind.sh file has been changed to work with Python2.7, as on the new Ubuntu VM

--- lind.sh
+++ lind.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # set -o xtrace
-trap 'python2.6 ${REPY_PATH}/repy/repy.py --safebinary ${REPY_PATH}/repy/restrictions.lind ${REPY_PATH}/repy/lind_server.py $@' INT TERM EXIT
+trap 'python ${REPY_PATH}/repy/repy.py --safebinary ${REPY_PATH}/repy/restrictions.lind ${REPY_PATH}/repy/lind_server.py $@' INT TERM EXIT
