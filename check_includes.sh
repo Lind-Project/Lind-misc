529f767e3653ee9ab0c91b58abd8fa1a1525f6b8
Artiom Baloian <artiom.baloian@nyu.edu>
2016-11-29 15:20:08 -0500

    Modified check_includes.sh file in order to recognize new file hierarchy in nacl_repy/seattlelib/.

--- check_includes.sh
+++ check_includes.sh
@@ -13,7 +13,20 @@ rc=0
 for file in $files 
 do
     real_f=$file_path/$file
-    if [ ! -f $real_f ]
+    fs_files=$file_path/fs/$file
+    net_files=$file_path/net/$file
+    sys_files=$file_path/sys/$file
+    lind_files=$file_path/lind/$file
+    xmlrpc_files=$file_path/xmplrpc/$file
+    librepy_files=$file_path/librepy/$file
+
+    if [ ! -f $real_f ] && \
+       [ ! -f $fs_files ] && \
+       [ ! -f $net_files ] && \
+       [ ! -f $sys_files ] && \
+       [ ! -f $lind_files ] && \
+       [ ! -f $xmlrpc_files ] && \
+       [ ! -f $librepy_files ]
     then
 	echo "Error: include $file is missing in $file_name"
 	rc=1
--- foreign/Python-2.7.3/build/temp.linux-x86_64-2.7/libffi/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/home/lind/lind/misc/foreign/Python-2.7.3/Modules/_ctypes/libffi/src/x86/ffitarget.h
