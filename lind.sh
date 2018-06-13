a8baf326eedc7d6c8e2f4ce392703ff8590b9ce5
Joey Pabalinas <joeypabalinas@gmail.com>
2018-06-13 11:45:56 -1000

    lind.sh: add default environment
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- lind.sh
+++ lind.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 # set -o xtrace
 
-if test -z "$REPY_PATH"; then
-  exit 1
-fi
+export LIND_BASE="${LIND_BASE:-/usr/lind_project}"
+export LIND_SRC="${LIND_SRC:-/usr/lind_project/lind}"
+export LIND_MONITOR="${LIND_MONITOR:-/usr/lind_project/reference_monitor}"
+export NACL_SDK_ROOT="${NACL_SDK_ROOT:-/usr/lind_project/lind/repy/sdk}"
+export REPY_PATH="${REPY_PATH:-/usr/lind_project/lind/repy}"
+export PYTHON="${PYTHON:-/usr/bin/python2}"
+export PNACLPYTHON="${PNACLPYTHON:-/usr/bin/python2}"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-/lib/glibc}"
 
 cwd="$PWD"
-cd "$REPY_PATH/repy" || exit 1
+trap 'cd "$cwd"' EXIT
 
-trap '{ cd "$cwd"; trap - INT; kill -INT "$$"; } ' INT
-trap '{ cd "$cwd"; trap - EXIT; kill -EXIT "$$"; } ' EXIT
+cd "$REPY_PATH/repy" || exit 1
 
 sel_ldr -a -- /lib/glibc/runnable-ld.so --library-path /lib/glibc "$@"
