4a36ac5e9ddfe0d151aea1d1ecec6615d2afe7fe
Joey Pabalinas <joeypabalinas@gmail.com>
2018-06-15 20:47:13 -1000

    lind.sh: add xtrace flag
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- lind.sh
+++ lind.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
 
-# set -o xtrace
+# check for xtrace flag
+while getopts :hx opt; do
+	case "$opt" in
+	x)
+		set -o xtrace
+		;;
+	h)
+		printf 'usage: %s [-x] <lind program path>\n' "${0##*/}" >&2
+		exit
+		;;
+	:|?)
+		printf 'illegal option letter: %s\n' "'$OPTARG'" >&2
+		printf 'usage: %s [-x] <lind program path>\n' "${0##*/}" >&2
+		exit 1
+		;;
+	esac
+done
+shift "$((OPTIND - 1))"
 
 export LIND_BASE="${LIND_BASE:-/usr/lind_project}"
 export LIND_SRC="${LIND_SRC:-/usr/lind_project/lind}"
 export LIND_MONITOR="${LIND_MONITOR:-/usr/lind_project/reference_monitor}"
 export NACL_SDK_ROOT="${NACL_SDK_ROOT:-/usr/lind_project/lind/repy/sdk}"
 export REPY_PATH="${REPY_PATH:-/usr/lind_project/lind/repy}"
-export PYTHON="${PYTHON:-/usr/bin/python2}"
-export PNACLPYTHON="${PNACLPYTHON:-/usr/bin/python2}"
+export PYTHON="${PYTHON:-python2}"
+export PNACLPYTHON="${PNACLPYTHON:-python2}"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-/lib/glibc}"
 
 cwd="$PWD"
